c28585164f89e585ee728530d4616fc05df1a17b
tradershubspace <hello.tradershub@gmail.com>
2025-03-12 09:16:28 +0100

    Add MetaTrader 5 client and factory implementations for data and execution

--- nautilus_mt5/client/__init__.py
+++ nautilus_mt5/client/__init__.py
@@ -1,6 +1,8 @@
 from .sockets import MetaTrader5StreamClient
+from .client import MetaTrader5Client
 
 __all__ = [
+    "MetaTrader5Client",
     "MetaTrader5StreamClient",
 ]
 
---
+++ nautilus_mt5/client/client.py
@@ -0,0 +1,8 @@
+
+class MetaTrader5Client:
+    """
+    Provides a IPC/RPYC/Sockets client for MetaTrader 5.
+    """
+    def __init__(self):
+        pass
+    
