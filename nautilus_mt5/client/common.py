a920787ede7735709c8ca2527840adf20e6e928b
tradershubspace <hello.tradershub@gmail.com>
2025-03-12 07:01:16 +0100

    Add initial implementation of TimeframeAggregator and error handling classes for MT5 client

--- nautilus_mt5/client/common.py
+++ nautilus_mt5/client/common.py
@@ -5,6 +5,10 @@ from abc import abstractmethod
 from collections.abc import Callable
 from decimal import Decimal
 from typing import Annotated, Any, NamedTuple
+from decimal import Decimal
+from enum import Enum
+import math
+import sys
 
 import msgspec
 from mt5 import (
@@ -22,6 +26,45 @@ from nautilus_trader.common.component import MessageBus
 from nautilus_trader.model.data import BarType
 from nautilus_trader.model.identifiers import InstrumentId
 
+
+NO_VALID_ID = -1
+MAX_MSG_LEN = 0xFFFFFF  # 16Mb - 1byte
+UNSET_INTEGER = 2**31 - 1
+UNSET_DOUBLE = sys.float_info.max
+UNSET_LONG = 2**63 - 1
+UNSET_DECIMAL = Decimal(2**127 - 1)
+DOUBLE_INFINITY = math.inf
+
+class TerminalConnectionMode(Enum):
+    """Terminal Connection Mode type.
+    
+    Includes 3 client modes: IPC, EA, and EA_IPC.
+
+    IPC: Mode for MetaTrader IPC communication on Windows.
+    EA: Mode for EA communication using sockets.
+    EA_IPC: Mode for combining EA and IPC communication (EA for streaming and IPC for request-reply).
+    
+    Since RPYC is used for IPC communication in Linux, it will be used for IPC mode in Linux (MetaTrader RPYC communication on Linux).
+    """
+    IPC = "IPC"
+    EA = "EA"
+    EA_IPC = "EA_IPC"
+
+    def to_str(self) -> str:
+        """Returns the string representation of the enum value."""
+        return self.value
+    
+class TerminalPlatformType(Enum):
+    """Terminal Platform type.
+    
+    Includes 2 platform types: WINDOWS and LINUX.
+    """
+    WINDOWS = "Windows"
+    LINUX = "Linux"
+
+    def to_str(self) -> str:
+        """Returns the string representation of the enum value."""
+        return self.value
 
 class AccountOrderRef(NamedTuple):
     account_id: str  # Account ID/Login Number of the account
--- nautilus_mt5/common.py
+++ nautilus_mt5/common.py
@@ -1,51 +1,9 @@
-from decimal import Decimal
-from enum import Enum
-import math
-import sys
 from typing import Final, Literal, Optional
 from nautilus_trader.config import NautilusConfig
 from nautilus_trader.model.identifiers import Venue
 
-NO_VALID_ID = -1
-MAX_MSG_LEN = 0xFFFFFF  # 16Mb - 1byte
-UNSET_INTEGER = 2**31 - 1
-UNSET_DOUBLE = sys.float_info.max
-UNSET_LONG = 2**63 - 1
-UNSET_DECIMAL = Decimal(2**127 - 1)
-DOUBLE_INFINITY = math.inf
 MT5_VENUE: Final[Venue] = Venue("METATRADER_5")
 
-class TerminalConnectionMode(Enum):
-    """Terminal Connection Mode type.
-    
-    Includes 3 client modes: IPC, EA, and EA_IPC.
-
-    IPC: Mode for MetaTrader IPC communication on Windows.
-    EA: Mode for EA communication using sockets.
-    EA_IPC: Mode for combining EA and IPC communication (EA for streaming and IPC for request-reply).
-    
-    Since RPYC is used for IPC communication in Linux, it will be used for IPC mode in Linux (MetaTrader RPYC communication on Linux).
-    """
-    IPC = "IPC"
-    EA = "EA"
-    EA_IPC = "EA_IPC"
-
-    def to_str(self) -> str:
-        """Returns the string representation of the enum value."""
-        return self.value
-    
-class TerminalPlatformType(Enum):
-    """Terminal Platform type.
-    
-    Includes 2 platform types: WINDOWS and LINUX.
-    """
-    WINDOWS = "Windows"
-    LINUX = "Linux"
-
-    def to_str(self) -> str:
-        """Returns the string representation of the enum value."""
-        return self.value
-    
 class MT5Symbol(NautilusConfig, frozen=True, repr_omit_defaults=True):
     """
     Class describing an instrument's definition.
